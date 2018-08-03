4efe7e9e4fba6790d846e8c03c7d901319eb68ec
Arthur Schut <aschutgh@gmail.com>
2018-08-03 16:48:41 +0200

    Opgeven van meerdere boeken is nu mogelijk

--- GroenBibliotheek2/Program.cs
+++ GroenBibliotheek2/Program.cs
@@ -25,27 +25,65 @@ namespace GroenBibliotheek2
 
     class Program
     {
+
+        static string vraagUitleendatum()
+        {
+            Console.Write("Geef uitleendatum in formaat jjjj/mm/dd: ");
+            return Console.ReadLine();
+        }
+
+        static string vraagBoeknummer()
+        {
+            Console.Write("Geef boeknummer van vier cijfers: ");
+            return Console.ReadLine();
+        }
+
+        static string vraagRetourdatum()
+        {
+            Console.Write("Geef retourdatum in formaat jjjj/mm/dd: ");
+            return Console.ReadLine();
+        }
+
+        static void vraagBoekinfo(ref Dictionary<string, Boek> tboeken)
+        {
+            string boeknummer = vraagBoeknummer();
+            DateTime uitleendat = DateTime.Parse(vraagUitleendatum());
+            DateTime retourdat = DateTime.Parse(vraagRetourdatum());
+            var nieuwboek = new Boek(boeknummer, uitleendat, retourdat);
+            tboeken.Add(boeknummer, nieuwboek);
+
+        }
+
         static void Main(string[] args)
         {
-            var uitleendat = DateTime.Parse("2018/08/01");
-            var retourdat = DateTime.Parse("2018/08/03");
+            var tboeken = new Dictionary<string, Boek>();
+            Console.WriteLine("Welkom bij de bibliotheek!");
+            var meerboeken = true;
+            while (meerboeken == true)
+            {
+                vraagBoekinfo(ref tboeken);
+                Console.Write("Wil je meer boeken invoeren? (ja of nee) ");
+                if (Console.ReadLine() == "nee")
+                {
+                    meerboeken = false;
+                }
+            }
 
-            var boek = new Boek("9321", uitleendat, retourdat);
-            Console.WriteLine(boek.Bnummer);
-            Console.WriteLine(boek.Btype);
-            Console.WriteLine(boek.Uitleendat);
-            Console.WriteLine(boek.Retourdat);
-            Console.WriteLine(boek.Boete);
+            foreach (string boeknummer in tboeken.Keys)
+            {
+                Console.WriteLine("{0} / {1}; {2}; {3}; {4}, {5}", boeknummer, tboeken[boeknummer].Bnummer, tboeken[boeknummer].Btype, tboeken[boeknummer].Uitleendat, tboeken[boeknummer].Retourdat, tboeken[boeknummer].Boete);
+            }
 
-            uitleendat = DateTime.Parse("2018/07/01");
-            retourdat = DateTime.Parse("2018/07/29");
+            //var uitleendat = DateTime.Parse("2018/08/01");
+            //var retourdat = DateTime.Parse("2018/08/03");
 
-            boek = new Boek("3321", uitleendat, retourdat);
-            Console.WriteLine(boek.Bnummer);
-            Console.WriteLine(boek.Btype);
-            Console.WriteLine(boek.Uitleendat);
-            Console.WriteLine(boek.Retourdat);
-            Console.WriteLine(boek.Boete);
+            //var boek = new Boek("9321", uitleendat, retourdat);
+            //Console.WriteLine(boek.Bnummer);
+            //Console.WriteLine(boek.Btype);
+            //Console.WriteLine(boek.Uitleendat);
+            //Console.WriteLine(boek.Retourdat);
+            //Console.WriteLine(boek.Boete);
+
         }
     }
 }
